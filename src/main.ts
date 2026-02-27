9bbc394d21cd63cf0ce00ce84568401fc9b86738
asim sapkota <buffbahun@asims-MacBook-Air.local>
2026-02-27 20:49:41 +0545

    barcode and media media-stream api test

---
+++ src/main.ts
@@ -0,0 +1,68 @@
+import './style.css'
+
+// const appRoot = document.querySelector<HTMLDivElement>('#app');
+
+let barcodeDetector: BarcodeDetector | null = null;
+
+if (!("BarcodeDetector" in globalThis)) {
+  console.log("Barcode Detector is not supported by this browser.");
+} else {
+  console.log("Barcode Detector supported!");
+
+  // create new detector
+  barcodeDetector = new BarcodeDetector({
+    formats: ["qr_code"],
+  });
+}
+
+const video = document.getElementById('video') as HTMLVideoElement | null;
+navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } }) // Use the rear camera
+    .then(stream => {
+        if (video) {
+        video.srcObject = stream;
+        video.play();
+        video.addEventListener('loadeddata', () => onVideoLoaded(stream));
+      }
+    })
+    .catch(err => {
+        console.error('Error accessing camera:', err);
+});
+
+let decoding = false;
+
+function onVideoLoaded(stream: MediaStream) {
+  startDecoding(stream);
+}
+
+function startDecoding(stream: MediaStream) {
+    // Scan every 100 milliseconds (adjust as needed)
+    const intervalId = setInterval(async () => {
+        if (decoding) return;
+        decoding = true;
+        console.log("here");
+        try {
+            if (!barcodeDetector || !video) return;
+            const barcodes = await barcodeDetector.detect(video); // Pass the video element directly
+            barcodes.forEach(barcode => {
+                console.log('Barcode detected:', barcode.rawValue);
+                stream.getTracks().forEach(trk => trk.stop());
+                // Process the result here
+            });
+            if (stream.getTracks().every(trk => trk.readyState === "ended")) {
+              clearInterval(intervalId);
+            }
+        } catch (e) {
+            console.error('Barcode detection failed:', e);
+        } finally {
+            decoding = false;
+        }
+    }, 100);
+}
+
+const stopBtn = document.getElementById("btn");
+
+stopBtn?.addEventListener("click", () => {
+  const srcObj = video?.srcObject as MediaStream | null | undefined;
+  srcObj?.getTracks()?.forEach(trk => trk.stop());
+  console.log(srcObj?.getTracks())
+})
---
+++ src/types/barcode.d.ts
@@ -0,0 +1,16 @@
+interface BarcodeDetectorOptions {
+    formats?: string[];
+  }
+  
+  interface DetectedBarcode {
+    boundingBox: DOMRectReadOnly;
+    rawValue: string;
+    format: string;
+    cornerPoints: { x: number; y: number }[];
+  }
+  
+  declare class BarcodeDetector {
+    constructor(options?: BarcodeDetectorOptions);
+    detect(image: ImageBitmapSource): Promise<DetectedBarcode[]>;
+    static getSupportedFormats(): Promise<string[]>;
+  }
